a28ee220f824f64ebe6c469f9d5d0798a2bd67d3
Abdelrahman Sayed <78025280+abdelrahmansayed55@users.noreply.github.com>
2023-07-27 18:43:04 +0300

    environ.c

---
+++ environ.c
@@ -0,0 +1,89 @@
+#include "shell.h"
+
+/**
+ * _myenv - this prints the current environment
+ * @info: this structure containing potency args.to maintain const function prototype.
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * _getenv - gets the value of an environment var
+ * @info:  this structure contains potency args. 
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *q;
+
+	while (node)
+	{
+		q = starts_with(node->str, name);
+		if (q && *q)
+			return (q);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - this Initialize a new environ var,
+ *             or modify an existing one
+ * @info: this structure contains potency args.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguments\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - this remove an environ var
+ * @info: this structure contains potency args. Used to maintain function prototype.
+ * Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguments.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: this struct containing potency args
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
+
